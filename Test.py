858d6d14a38d378fc9e295609044c64658a48e81
Thorley-Joyce <Rachel.Thorley-Joyce@ons.gov.uk>
2023-06-01 13:58:40 +0100

    Updated to add code

--- Test.py
+++ Test.py
@@ -9,3 +9,19 @@ conda install -c conda-forge pyspark
 conda install -c "conda-forge/label/cf201901" pyspark
 conda install -c "conda-forge/label/cf202003" pyspark
 
+def add_two_numbers(first_number, second_number):
+    """
+    The sum of two numbers.
+
+    Parameters
+    ----------
+    first_number : int or float
+        Number to add
+    second_number : int or float
+        Number to add
+
+    Returns
+    -------
+    int or float
+    """
+    return first_number + second_number
