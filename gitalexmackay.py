69e5fb2c746a6325e6c400dc7b7d5a20d8e82c80
Mackay <Alex.Mackay@ons.gov.uk>
2023-06-01 13:54:34 +0100

    Add multiply two numbers to branch'

---
+++ gitalexmackay.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+def multiply_two_numbers(first_number, second_number):
+    """
+    The product of two numbers.
+
+    Parameters
+    ----------
+    first_number : int or float
+        Number to multiply
+    second_number : int or float
+        Number to multiply
+
+    Returns
+    -------
+    int or float
+    """
+    return first_number * second_number
+
