0d428d537d0aff7b7c8ceed10b8e20566e40ad40
Phil Yeeles <phil@yeel.es>
2023-06-01 13:42:21 +0100

    Add function to subtract two numbers

---
+++ demo_subtract_py_as.py
@@ -0,0 +1,14 @@
+def subtract(first_number, second_number):
+    """
+    Subtract first_number from second_number
+
+    Parameters
+    ----------
+    first_number: Number
+    second_number: Number
+
+    Returns
+    -------
+    first_number - second_number (int, float &c.)
+    """
+    return first_number - second_number
