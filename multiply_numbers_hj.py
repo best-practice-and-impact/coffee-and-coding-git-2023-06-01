dc1b4ad552b4396b71131dc76b847792a2d2b0ff
Hall <Johanna.Hall@ons.gov.uk>
2023-06-01 13:52:23 +0100

    adding multiplication function

---
+++ multiply_numbers_hj.py
@@ -0,0 +1,17 @@
+def multiply_numbers(first_number, second_number):
+    """
+    The multiplication of two numbers
+
+    Parameters
+    -----------
+    first_number: int or float
+        Number to add 
+    second_number: int or float
+        Number to add
+    
+    Returns:
+    --------
+    int or float
+    
+    """
+    return first_number*second_number
